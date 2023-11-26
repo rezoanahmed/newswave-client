ca5416923ee0ef44e1898cf19d1d237f84dafccb
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-11-26 12:08:57 +0600

    changed contents in features section

--- src/pages/Features/Features.jsx
+++ src/pages/Features/Features.jsx
@@ -3,25 +3,24 @@ const Features = () => {
     return (
         <div>
             <section class="text-gray-600 body-font">
-                <div class="container px-5 py-24 mx-auto">
-                    <div class="text-center mb-20">
-                        <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">Raw Denim Heirloom Man Braid</h1>
-                        <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-500s">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug.</p>
+                <div class="container px-5 py-10 mx-auto">
+                    <div class="text-center mb-10">
+                        
                         <div class="flex mt-6 justify-center">
-                            <div class="w-16 h-1 rounded-full bg-indigo-500 inline-flex"></div>
+                            <div class="w-16 h-1 rounded-full bg-blue inline-flex"></div>
                         </div>
                     </div>
                     <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
                         <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-                            <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+                            <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-blue mb-5 flex-shrink-0">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
                                     <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
                                 </svg>
                             </div>
                             <div class="flex-grow">
-                                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Shooting Stars</h2>
-                                <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
-                                <a class="mt-3 text-indigo-500 inline-flex items-center">Learn More
+                                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Exclusive Content for Subscribers</h2>
+                                <p class="leading-relaxed text-base">Provide premium content or early access to subscribers</p>
+                                <a class="mt-3 text-blue inline-flex items-center">Learn More
                                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                         <path d="M5 12h14M12 5l7 7-7 7"></path>
                                     </svg>
@@ -29,7 +28,7 @@ const Features = () => {
                             </div>
                         </div>
                         <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-                            <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+                            <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-blue mb-5 flex-shrink-0">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
                                     <circle cx="6" cy="6" r="3"></circle>
                                     <circle cx="6" cy="18" r="3"></circle>
@@ -37,9 +36,9 @@ const Features = () => {
                                 </svg>
                             </div>
                             <div class="flex-grow">
-                                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">The Catalyzer</h2>
-                                <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
-                                <a class="mt-3 text-indigo-500 inline-flex items-center">Learn More
+                                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">User-Generated Content</h2>
+                                <p class="leading-relaxed text-base">Allow users to submit their own stories, photos, or opinions</p>
+                                <a class="mt-3 text-blue inline-flex items-center">Learn More
                                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                         <path d="M5 12h14M12 5l7 7-7 7"></path>
                                     </svg>
@@ -47,16 +46,16 @@ const Features = () => {
                             </div>
                         </div>
                         <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-                            <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
+                            <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-blue mb-5 flex-shrink-0">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
                                     <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
                                     <circle cx="12" cy="7" r="4"></circle>
                                 </svg>
                             </div>
                             <div class="flex-grow">
-                                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Neptune</h2>
-                                <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
-                                <a class="mt-3 text-indigo-500 inline-flex items-center">Learn More
+                                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Social Media Integration</h2>
+                                <p class="leading-relaxed text-base">Facilitate easy sharing of articles on social media platforms</p>
+                                <a class="mt-3 text-blue inline-flex items-center">Learn More
                                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                         <path d="M5 12h14M12 5l7 7-7 7"></path>
                                     </svg>
@@ -64,7 +63,7 @@ const Features = () => {
                             </div>
                         </div>
                     </div>
-                    <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                    
                 </div>
             </section>
         </div>
