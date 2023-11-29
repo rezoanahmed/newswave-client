13ccc04f7061d97220e485c632da79b0ab82cae9
Rezoan Ahmed <rezoan.cse.2017@gmail.com>
2023-11-29 12:53:35 +0600

    used tanstack query in home articles section

--- src/pages/AllArticles/AllArticlesHome.jsx
+++ src/pages/AllArticles/AllArticlesHome.jsx
@@ -3,9 +3,11 @@ import { useState } from "react";
 import ArticleCard from "./ArticleCard";
 import useAxiosPublic from "../../hooks/useAxios";
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import LoadingAnimation from "../../components/LoadingAnimation";
 
 const AllArticlesHome = () => {
-    const [article, setArticle] = useState([]);
+    // const [article, setArticle] = useState([]);
     // useEffect(()=>{
     //     fetch("/posts.json")
     //     .then(res=>res.json())
@@ -14,13 +16,23 @@ const AllArticlesHome = () => {
     //     })
     // },[])
     const axiosPublic = useAxiosPublic();
-    useEffect(() => {
-        axiosPublic.get("/articles?status=approved")
-            .then(data => {
-                setArticle(data.data);
-            })
-    }, [])
+    // useEffect(() => {
+    //     axiosPublic.get("/articles?status=approved")
+    //         .then(data => {
+    //             setArticle(data.data);
+    //         })
+    // }, [])
     // console.log(article);
+    const {data:article, isLoading} = useQuery({
+        queryKey: "articleForHome",
+        queryFn: async()=>{
+            const result = await axiosPublic.get("/articles?status=approved")
+            return result?.data;
+        }
+    })
+    if(isLoading){
+        return <LoadingAnimation></LoadingAnimation>
+    }
     return (
         <>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 p-2 md:p-16">
